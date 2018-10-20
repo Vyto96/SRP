77c00d56e6fcd5701845a5cac54f9664efe281a2
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-10-20 21:52:07 +0200

    prova con altro account

--- sr/app/middleware/ebay.py
+++ sr/app/middleware/ebay.py
@@ -49,4 +49,4 @@ def ebay_auth_code_response():
     # s = Store(store_name='ebay.it', auth_code=cod)
     # db.session.add(s)
     # db.session.commit()
-    return '<h1>not encoded:<br> {} <br> code inviato: <br> {}<br> json ricevuto == <br> {}</h1>'.format(cod_not_encoded, auth_cod, response.json())
+    return '<h1> {} <br> json ricevuto == <br> {}</h1>'.format(response.text, response.json())
