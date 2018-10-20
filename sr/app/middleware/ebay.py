47e00105d10a179a0246e594b542c2c8c7f68288
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-10-20 19:03:45 +0200

    test response obj

--- sr/app/middleware/ebay.py
+++ sr/app/middleware/ebay.py
@@ -44,5 +44,5 @@ def ebay_auth_code_response():
     # s = Store(store_name='ebay.it', auth_code=cod)
     # db.session.add(s)
     # db.session.commit()
-
-    return '<h1>json ricevuto == <br> {}</h1>'.format(response.json())
+    response.
+    return '<h1>json ricevuto == <br> {}</h1>'.format(response.headers)
