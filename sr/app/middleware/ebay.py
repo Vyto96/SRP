9cf426d257e8347f65ed84bfa84bca8599f28ca9
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-10-29 20:45:29 +0100

    rotta per provare vyto user

--- sr/app/middleware/ebay.py
+++ sr/app/middleware/ebay.py
@@ -1,6 +1,4 @@
 from flask import jsonify, redirect, request, json
-from .. import db
-from ..models import Store
 from . import middle
 import requests, os
 from urllib.parse import unquote, quote
@@ -63,7 +61,8 @@ def ebay_refresh_token():
                 'scope': os.environ.get('EBAY_SCOPE_LIST')
     }
 
-    resp = json.loads( requests.post(url, data=payload, headers=headers).text )
+    # resp = json.loads( requests.post(url, data=payload, headers=headers).text )
+    resp = requests.post(url, data=payload, headers=headers).json()
 
     return jsonify( access_token=resp['access_token'] )
 
@@ -71,7 +70,7 @@ def ebay_refresh_token():
 
 
 @middle.route('/ebay/prova')
-def hello():
+def prova():
 
     UAtoken = {
         "access_token":"v^1.1#i^1#f^0#r^0#p^3#I^3#t^H4sIAAAAAAAAAOVYW2wUVRjutNuSgqVKiBfAuEzVIGR2z8zsbUZ245a2ttDL0l0aRBDOzJxpp52dWWbObLsQpZZAYipEkURCSC1BEonESAIaI1ESHySQSOKDxoSoIRK0gWJoFCMQndm2y7aGS1seNnFfduc///X7v//sOQN6y8qX7qzfeb2CmFU82At6iwmCngPKy0qXzS0pXlBaBPIUiMHep3tdfSW/LjdhUk3xrchM6ZqJ3D1JVTP5rDBMWobG69BUTF6DSWTyWOTj0aZGnvEAPmXoWBd1lXQ31IRJGiBZCAkikCCURQbYUm3cZ0IPk1JAZhDnC9LI+YFEe900LdSgmRhqOEwygA5RNKAYLkFzPMvxPuAJBZh1pLsNGaaia7aKB5CRbLp81tbIy/XuqULTRAa2nZCRhmhdvCXaUFPbnFjuzfMVGcMhjiG2zIlPK3QJudugaqG7hzGz2nzcEkVkmqQ3MhpholM+Op7MNNLPQs1JNMtwCAggyAowiB4IlHW6kYT47nk4EkWi5KwqjzSs4My9ELXREDqRiMeemm0XDTVu52u1BVVFVpARJmuroy+tide2ku54LGboaUVCUpZUPgb4AMcEGTKSzmCdC4xFGHUzhu+kECt0TVIctEx3s46rkZ0umgwKkweKrdSitRhRGTup5Okx9Dh4/tA6p5uj7bNwh+Y0FCVtBNzZx3tDP86F291/UGxgod8nAA76BU7y+cTQndjgzPpUGBFxmhKNxbxOLkiAGSoJjS6EUyoUESXa8FpJZCgSz/plhg3JiJICnEz5OFmmBL8UoGgZIYCQIIhc6H9BDIwNRbAwypFj8kK2ujDpgMkrUOax3oW0RCaFyMma2c1mjBE9ZpjswDjFe73d3d2ebtajG+1eBgDau7apMS52oCQkc7rKvZUpJcsN0d44bH0e2wmEyR6benZwrZ2MtNbWtdbG6zcmWlbVNo/TdkJmkcnSO1QaF/UUiumqImYKq0TWkGLQwJk4UlVbMKMiTafIwirPmXXHh2k7gSnF4zDOI+pJrw7t7coRbcxm7b4fJa9pg+QZHX7bu8dAUNI1NTMd4ynYKFraHiHdyEwnYM54CjZQFHVLw9MJN2Y6BQvZUmVFVZ1dYjoB88ynkqYG1QxWRDMXckbEj6ZSDcmkhaGgogapsCaABYw/96cw/fIKrKq0gm1SKzpln73TkIq11lCsM+sc6w8FmZCPggEpEAyI4owKb2pXCqxu2g8CHE2zgAOAmVFtNShdaE1lBRaEQFCiWIQ4yodEPwV9HEP5oV9GgBX8TMg/o5pXqIq9TxTeOaNeNzGSZlaafRIurKKcmRwfRzHgt8+L9mWX8klygBJ8Ik2xDH3f3ZwkiGPX0tzJ8j+3Ce/Ee3ykKPuh+4gToI84VkwQwAueoavA4rKSNa6ShxaYCkYe+xTqMZV2zb6eGsjThTIpqBjFZcTLiz4+sjHvzcHgBvB47t1BeQk9J+9FAlh0e6WUrnysgg7RgOFojuV8YB2our3qoh91zf/+6qmv/iw5VvFa28m/tm1t7NjCDp8DFTklgigtcvURRbt2f3Ag9Mq+TZ0bOg8NfQHIlWe3rY4M/36r/N3ZPQtXvnll9yOp7sUjI1XcocqBIXBtJDPv3PlPOw9bFVe2tH47nDaDpwe2X/ptQ1vlbP/Fxuv/vBGoOU1cnf/zH3vXvz2XuGR5yM+XnNiz/cXnH95f1xlM7Hjvo76TVU88uXnwuV9W9y06+oly63h1+tRTZP8Lu7c3ndWbnj187cIRInFw6HKE2gVvfbnsB544vKt/yaybZ965MbwgvoQ+ldg8Z8/5gyeW3xhBF183tokDsdk/9V/9en/TzX3HXw3s3TSw5puhrZfBhzvQZwsvrj3jcx2tk690vd+//4JUb61aG5i3/sDf3/1Y6Wp+Y/Nb1UOj7fsX2vcjldMRAAA=",
@@ -121,7 +120,7 @@ def ebay_get_report():
     tk = request.headers.get('token')
 
     if tk:
-        mktp = request.args['marketplace'] #request.headers.get
+        mktp = request.args['marketplace']
         start_date = request.args['start_date']
         end_date = request.args['end_date']
 
