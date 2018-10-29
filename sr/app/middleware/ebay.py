6cc8de9796ef4423d7d9bf2f8ffd7c58cbbdb645
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-10-29 16:27:24 +0100

    param get by url in get_report 3

--- sr/app/middleware/ebay.py
+++ sr/app/middleware/ebay.py
@@ -66,9 +66,9 @@ def ebay_get_report():
 
     tk = request.headers.get('token')
 
-    mktp = request.args.['marketplace'] #request.headers.get
-    start_date = request.args.['start_date']
-    end_date = request.args.['end_date']
+    mktp = request.args['marketplace'] #request.headers.get
+    start_date = request.args['start_date']
+    end_date = request.args['end_date']
 
 
     url_api = 'https://api.ebay.com/sell/analytics/v1/traffic_report'
