9f35f4949cc5d545b5b5a507c28eb1e166068532
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-02 11:47:58 +0100

    chrome custom tabs attempt 1

--- sr/app/api/__init__.py
+++ sr/app/api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, abort
+from flask import Blueprint, request, abort, url_for, redirect
 import os
 api = Blueprint('api', __name__)
 
@@ -8,6 +8,19 @@ def before_request():
     req_api_key = request.headers.get('api_key')
     if  req_api_key is None or req_api_key != api_key:
         return 'api_key not valid', 401
+
+
+
+# prova redirect
+@api.route('/prova_redirect')
+def prova_redirect():
+    return 'se sei qui, sei stato redirectato'
+
+# prova redirect
+@api.route('/')
+def index():
+    return redirect(url_for('api.prova_redirect', _external=True)) 
+
 
 #import dei file .py che contengono le view fun che ritornano le risorse indicate dal nome del file stesso
 from . import users, stores, ecommerces, functions, prova
