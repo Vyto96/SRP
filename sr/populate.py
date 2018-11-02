62d0976bac612826adf649abff11e2e0d91efc1b
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-02 19:54:55 +0100

    default function & ecommerce script added populate.py 2

--- sr/populate.py
+++ sr/populate.py
@@ -2,5 +2,6 @@ from sr import app
 from app import db
 from app.models import User, Store, Ecommerce, Function
 
-Ecommerce.insert_all()
-Function.insert_all()
+with app.app_context():
+    Ecommerce.insert_all()
+    Function.insert_all()
