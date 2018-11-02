0a04244fb2f1115947c9c0443963fb872dcd4a9a
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-02 03:35:45 +0100

    before request hook added for api key control 2

--- sr/app/api/__init__.py
+++ sr/app/api/__init__.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
 import os
 api = Blueprint('api', __name__)
 
 @api.before_request
 def before_request():
     api_key = os.environ.get('MY_API_KEY')
-    if request.headers.get('api_key') is not api_key:
-        return 'API key not valid', 401
+    req_api_key = request.headers.get('api_key')
+    if  req_api_key is None or req_api_key is not api_key:
+        abort(401)
 
 #import dei file .py che contengono le view fun che ritornano le risorse indicate dal nome del file stesso
 from . import users, stores, ecommerces, functions, prova
