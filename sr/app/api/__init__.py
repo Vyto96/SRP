da90c618d6bf8b43047bbf159158d0fb752c08ec
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-02 03:43:59 +0100

    before request hook added for api key control 6

--- sr/app/api/__init__.py
+++ sr/app/api/__init__.py
@@ -6,7 +6,7 @@ api = Blueprint('api', __name__)
 def before_request():
     api_key = os.environ.get('SR_API_KEY')
     req_api_key = request.headers.get('api_key')
-    if  req_api_key is None or req_api_key is not api_key:
+    if  req_api_key is None or req_api_key != api_key:
         return 'api_key not valid-->{} <br> valid_key->{}'.format(req_api_key, api_key), 401
 
 #import dei file .py che contengono le view fun che ritornano le risorse indicate dal nome del file stesso
