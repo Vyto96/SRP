ad770dc46197d0ab3e609a0864ca098e798e2d2d
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-02 19:48:16 +0100

    default function & ecommerce da fare a mano 3

--- sr/sr.py
+++ sr/sr.py
@@ -4,8 +4,7 @@ from app.models import User, Store, Ecommerce, Function
 
 
 app = create_app()
-Ecommerce.insert_all()
-Function.insert_all()
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=4000)
