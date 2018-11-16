f3fa213c428d1912f615307dae46593decf78252
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-16 05:04:53 +0100

    caddyfile update for Vue integration att1

--- sr/app/web_app/main_view.py
+++ sr/app/web_app/main_view.py
@@ -1,8 +1,22 @@
 from . import web_app
 import requests
-from flask import url_for, redirect
+from flask import url_for, redirect, render_template
 
-@web_app.route('/prova_web')
-def index():
-    r = requests.get(url_for('api.get_users', _external=True) )
-    return '</h1>ritorno dell api--></h1><br>' + str(r.text)
+
+
+# 
+# @web_app.route('/')
+# def index():
+#     return render_template
+#
+#
+
+
+
+
+
+
+# @web_app.route('/prova_web')
+# def prova_web():
+#     r = requests.get(url_for('api.get_users', _external=True) )
+#     return '</h1>ritorno dell api--></h1><br>' + str(r.text)
