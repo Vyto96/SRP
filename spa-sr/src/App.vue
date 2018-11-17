2965e10786e4b38c6594df1adcb4c0157bcd4de0
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-17 21:41:29 +0100

    header component for navbar

--- spa-sr/src/App.vue
+++ spa-sr/src/App.vue
@@ -1,43 +1,15 @@
 <template>
   <div id="app">
-
 <!-- NAVBAR -->
-      <b-navbar toggleable="md" type="dark" variant="dark">
-
-        <b-nav-toggle target="nav_collapse"></b-nav-toggle>
-
-        <b-link class="navbar-brand" to="#">
-          <span>SalesReporter</span>
-        </b-link>
-
-        <b-collapse is-nav id="nav_collapse">
-
-          <!-- COLLAPSE ITEM -->
-          <b-navbar-nav>
-
-            <b-nav-item>
-              <router-link to="/">Home</router-link>
-            </b-nav-item>
-            <b-nav-item>Link
-              <router-link to="/about">About SR</router-link>
-            </b-nav-item>
-          </b-navbar-nav>
-
-
-          <!-- END COLLAPSE ITEM -->
-
-        </b-collapse>
-
-      </b-navbar>
-
-
+    <sr-header></sr-header>
 <!--ROUTER VIEW -->
-
     <router-view/>
-
-
   </div>
 </template>
+
+<script>
+
+</script>
 
 <style lang="scss">
 
--- spa-sr/src/main.js
+++ spa-sr/src/main.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// BOOTSTRAP STUFF
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// MY COMPONENTS
+import SR-header from './components/Header.vue'
+
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+Vue.component('sr-header', SR-header);
 
 new Vue({
   router,
