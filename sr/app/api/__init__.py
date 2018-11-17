d86b0cede3f229cc46a5061f9bdcf469e064888f
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-18 00:57:36 +0100

    COMMENTO DELL APY_KEY BEFORE REQ

--- sr/app/api/__init__.py
+++ sr/app/api/__init__.py
@@ -2,12 +2,12 @@ from flask import Blueprint, request, abort, url_for, redirect, session
 import os
 api = Blueprint('api', __name__)
 
-@api.before_request
-def before_request():
-    api_key = os.environ.get('SR_API_KEY')
-    req_api_key = request.headers.get('api_key')
-    if  req_api_key is None or req_api_key != api_key:
-        return 'api_key not valid', 401
+# @api.before_request
+# def before_request():
+#     api_key = os.environ.get('SR_API_KEY')
+#     req_api_key = request.headers.get('api_key')
+#     if  req_api_key is None or req_api_key != api_key:
+#         return 'api_key not valid', 401
 
 
 
