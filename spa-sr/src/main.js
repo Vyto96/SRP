f9de1a51e655186646c173dc1885356bb3c12b89
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-17 13:01:21 +0100

    prima prova navbar

--- spa-sr/src/main.js
+++ spa-sr/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-bue.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
