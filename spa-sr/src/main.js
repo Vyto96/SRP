6b47c5355ebd83fffb1a22209ddbe50019e6fa46
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-17 21:48:58 +0100

    header component for navbar

--- spa-sr/src/main.js
+++ spa-sr/src/main.js
@@ -7,13 +7,13 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 // MY COMPONENTS
-import SR-header from './components/Header.vue'
+import SRHeader from './components/Header.vue'
 
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
-Vue.component('sr-header', SR-header);
+Vue.component('sr-header', SRHeader);
 
 new Vue({
   router,
