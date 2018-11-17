3490bb66fb4ea8d88775b8ffca7adc227308891e
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-17 12:59:33 +0100

    prima prova navbar

--- spa-sr/src/App.vue
+++ spa-sr/src/App.vue
@@ -1,29 +1,59 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+<!-- NAVBAR -->
+
+      <b-navbar toggleable type="inverse" variant="success">
+
+        <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+        <b-link class="navbar-brand" to="#">
+          <span>SalesReporter</span>
+        </b-link>
+
+        <b-collapse is-nav id="nav_collapse">
+
+          <!-- COLLAPSE ITEM -->
+
+          <b-nav-item>
+            <router-link to="/">Home</router-link> |
+          </b-nav-item>
+          <b-nav-item>Link
+            <router-link to="/about">About SR</router-link>
+          </b-nav-item>
+
+
+          <!-- END COLLAPSE ITEM -->
+
+        </b-collapse>
+
+      </b-navbar>
+
+
+<!--ROUTER VIEW -->
+
     <router-view/>
+
+
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+// #nav {
+//   padding: 30px;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
