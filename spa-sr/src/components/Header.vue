200c26a4b749606ed58d7c62bae6c4c2a869f697
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-17 21:54:44 +0100

    header component for navbar

---
+++ spa-sr/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+
+          <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+      <b-link class="navbar-brand" to="#">
+        <span>SalesReporter</span>
+      </b-link>
+
+      <b-collapse is-nav id="nav_collapse">
+        <!-- COLLAPSE ITEM -->
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link to="/">Home</router-link>
+          </b-nav-item>
+          <b-nav-item>Link
+            <router-link to="/about">About SR</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data: () => ({
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
