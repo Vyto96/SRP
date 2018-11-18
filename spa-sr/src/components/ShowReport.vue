005950dc1685e8177516db091d542b82f81fd79b
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-18 11:39:25 +0100

    prova report form

--- spa-sr/src/components/ShowReport.vue
+++ spa-sr/src/components/ShowReport.vue
@@ -1,6 +1,27 @@
 <template>
-  <div id="">
+  <div id="show-report">
       <h1>Show report for store with id = {{ id }}</h1>
+
+      <div>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group id="startDateGroup"
+                        label="Report start date:"
+                        label-for="startDate"
+                        description="Insert Start date of report.">
+            <b-form-input v-model="start-date"
+                         type="date"
+                         placeholder="Enter date"
+                         >
+           </b-form-input>
+
+           <b-button type="submit" variant="primary">Submit</b-button>
+           <b-button type="reset" variant="danger">Reset</b-button>
+          </b-form-group>
+        </b-form>
+
+
+
+
   </div>
 </template>
 <script>
