f7c4b09b1b073911ca7de28d896433f7f30e6325
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2018-11-18 11:12:35 +0100

    prova show-report

---
+++ spa-sr/src/components/ShowReport.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="">
+      <h1>Show report for store with id = {{ id }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: ['id'],
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
